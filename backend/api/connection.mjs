b9fc1d0650c41cd29558519282c5aac011f73f89
ines <inesmrad20@gmail.com>
2024-03-12 14:05:30 +0100

    Sign up + login APIs

---
+++ backend/api/connection.mjs
@@ -0,0 +1,51 @@
+import cors from 'cors';
+import express from 'express';
+
+import { postUser } from './db.mjs';
+import { getUserByEmail } from './db.mjs';
+// Allow requests from the Expo tunnel URL
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    // Call the postProvider function to add a new provider
+    await postUser(name, email, password);
+    res.status(201).json({ message: 'user added successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      res.status(401).json({ message: 'Invalid credentials' });
+      return;
+    }
+
+    // Check if the provided password matches the stored password
+    if (password === user.password) {
+      res.status(200).json({ message: 'Logged in successfully' });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+// Start the server
+app.listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
+});
