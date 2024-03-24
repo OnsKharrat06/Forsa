a116da9eb02553d51baa974d089bbf231d457f97
Lina Fadhli <133816965+luluna02@users.noreply.github.com>
2024-03-24 14:05:52 +0100

    get/put user, get matching, get favorites

--- backend/api/connection.mjs
+++ backend/api/connection.mjs
@@ -2,8 +2,7 @@ import cors from 'cors';
 import express from 'express';
 
 import { postUser } from './db.mjs';
-import { getUserByEmail } from './db.mjs';
-// Allow requests from the Expo tunnel URL
+import { getUserByEmail, getUserByID, updateUser, getMatchingPerUserID, getFavoritesPerUserID } from './db.mjs';
 
 
 const app = express();
@@ -22,6 +21,78 @@ app.post('/users', async (req, res) => {
   }
 });
 
+//get one user by ID
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await getUserByID(id);
+    res.status(200).json({
+      message: 'User found',
+      user: user
+    });
+  } catch (error) {
+    console.error("Error on getting one user:", error);
+    res.status(500).json({ error: 'Error on getting one user' });
+  }
+});
+
+//Update user information  depending on the required data to be updated (not
+// necessarely all of the information all at once)
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const fieldsToUpdate = req.body; 
+  try {
+    const result = await updateUser(id, fieldsToUpdate);
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: 'User updated successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//matchings per user
+app.get('/matching/:userid', async (req, res) => {
+  const { userid } = req.params;
+  try {
+    const matchings = await getMatchingPerUserID(userid);
+    if (matchings.length > 0) {
+      res.status(200).json({
+        message: 'Matchings found',
+        matchings: matchings
+      });
+    } else {
+      res.status(404).json({ message: 'No matchings found for the given user' });
+    }
+  } catch (error) {
+    console.error("Error on getting matchings for the user:", error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//favorites per user
+app.get('/favorites/:userid', async (req, res) => {
+  const { userid } = req.params;
+  try {
+    const favorites = await getFavoritesPerUserID(userid);
+    if (favorites.length > 0) {
+      res.status(200).json({
+        message: 'Favorites found',
+        favorites: favorites
+      });
+    } else {
+      res.status(404).json({ message: 'No favorites found for the given user' });
+    }
+  } catch (error) {
+    console.error("Error on getting favorites for the user:", error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//login
 app.post('/login',cors(), async (req, res) => {
   const {email,password} = req.body;
   try {
@@ -40,6 +111,7 @@ app.post('/login',cors(), async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 // Start the server
 app.listen(8000, () => {
   console.log('Server is running on http://localhost:8000');
