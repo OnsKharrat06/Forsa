098928191e268d4e8242d4d78031e77eb4e7c928
ikrammt <145692307+ikrammt@users.noreply.github.com>
2024-05-09 22:37:03 +0100

    update JobDetails

--- frontend/components/jobdetails/JobDetails.js
+++ frontend/components/jobdetails/JobDetails.js
@@ -139,6 +139,7 @@ const JobDetails = ({ job, navigation }) => {
           </View>
         </Animated.View>
 
+
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Tabs
             tabs={tabs}
